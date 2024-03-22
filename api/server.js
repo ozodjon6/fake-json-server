78bc7e1937fad49959233fc322273aeb13f70ff9
ozodbekjumaev <ozodjon6@gmail.com>
2024-03-22 16:33:08 +0500

    fix some db json

--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
+/** @format */
 
-const server = jsonServer.create()
+// See https://github.com/typicode/json-server#module
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
 
 // Uncomment to allow write operations
 // const fs = require('fs')
@@ -12,20 +14,24 @@ const server = jsonServer.create()
 // const router = jsonServer.router(db)
 
 // Comment out to allow write operations
-const router = jsonServer.router('db.json')
+const fs = require("fs");
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+const router = jsonServer.router(db);
 
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
+server.use(middlewares);
 // Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
 server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+  console.log("JSON Server is running");
+});
 
 // Export the Server API
-module.exports = server
+module.exports = server;
